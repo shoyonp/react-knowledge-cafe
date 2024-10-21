32b0a1ef63fc8a568c003d04e04ee310e885387a
shoyonp <shoyonkumar143@gmail.com>
2024-10-21 23:12:40 +0600

    added blog component

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -8,9 +8,11 @@ const Blogs = () => {
       .then((res) => res.json())
       .then((data) => setBlogs(data));
   }, []);
-  return <div>
-    
-  </div>;
+  return (
+    <div className="md:w-2/3">
+      <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+    </div>
+  );
 };
 
 export default Blogs;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import profile from '../../assets/images/profile.png'
 const Header = () => {
-    return (
-        <div>
-              <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
-        </div>
-    );
+  return (
+    <header className='flex justify-between items-center p-4 mx-4 border-b-2'>
+      <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
+      <img src={profile} alt="" />
+    </header>
+  );
 };
 
 export default Header;
