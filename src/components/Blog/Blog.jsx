8af2567cbc56648672fca726e3906535b6a2a177
shoyonp <shoyonkumar143@gmail.com>
2024-10-22 02:09:10 +0600

    added author , title, hastags, author img

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,7 +1,39 @@
 import PropTypes from "prop-types";
 const Blog = ({ blog }) => {
-  console.log(blog);
-  return <div></div>;
+  const {
+    title,
+    cover,
+    author,
+    author_img,
+    posted_date,
+    reading_time,
+    hastags,
+  } = blog;
+  return (
+    <div>
+      <img src={cover} alt={`Cover picture of the title ${title}`} />
+      <div className="flex justify-between">
+        <div className="flex">
+          <img className="w-14" src={author_img} alt="" />
+          <div className="ml-6">
+            <h3 className="text-2xl">{author}</h3>
+            <p>{posted_date}</p>
+          </div>
+        </div>
+        <div>
+          <span>{reading_time} min read</span>
+        </div>
+      </div>
+      <h2 className="text-4xl">{title}</h2>
+      <p>
+        {hastags.map((hash, idx) => (
+          <span key={idx}>
+            <a href="">{hash}</a>{" "}
+          </span>
+        ))}
+      </p>
+    </div>
+  );
 };
 
 Blog.propTypes = {
